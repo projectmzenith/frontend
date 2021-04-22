5e85d626b6ed064a1dd5e5b8bde6d804a7a219e4
jilar <j.ilar1215@hotmail.com>
2021-04-21 19:04:36 -0700

    axios setup and simple example action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,6 @@
+import zenith from '../apis/zenith';
 import{ADD_ANNOUNCEMENT}  from './types'
+import history from '../history';
 
 export const addAnnouncment= (announcment) =>{
   return{
@@ -6,3 +8,20 @@ export const addAnnouncment= (announcment) =>{
     payload:announcment
   }
 }
+
+//example action creater using API
+export const createStream= (Values)=> async(dispatch, getState)=>{
+  try{
+    //example header, we may not have an Auth reducer but focus on the syntax here
+    //important part is "Bearer + the token"
+    const header = { headers:{Authorization: `Bearer ${getState().Auth.token}`}};
+    const response= await zenith.post('/endpoint', Values ,header);
+    dispatch({
+     type: ACTION_NAME,
+     payload:response.data
+    });
+    history.push('/')');
+  }catch(err){
+    console.log(err);
+  }
+};
---
+++ src/apis/zenith.js
@@ -0,0 +1,5 @@
+import axios from 'axios';
+
+export default axios.create({
+  baseURL:'base url name here'
+});
